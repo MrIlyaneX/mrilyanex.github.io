e80e39d1ff80a90ac51d48f013ff9dd1161cf620
mrilyanex <mistyurin.ilya.im@gmail.com>
2019-12-12 20:50:44 +0300

    877777777777777777777777777vue?

---
+++ src/ShowButton.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<button style="color: red; background-color: #151a23; border-color: black;" :value="show" @click="CloseBlock()"><big>{{ mess }}</big></button>
+	</div>
+</template>
+
+<script>
+	export default{
+		props: ['show'],
+		data(){
+			return{
+				mess: 'Сжать',
+				show: !'show'
+			}
+		},
+		methods: {
+			CloseBlock (event){
+				this.show = !event.target.value;
+				this.$emit('messChange', this.show);
+			}
+		}
+	}
+</script>
+
+<style>
+	*{
+		position: fixed;
+		z-index: 1000;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
-//import App from './App.vue'
+import App from './App.vue'
 import App1 from './App1.vue'
 import Block from './Block.vue'
-//import Example from './Example.vue'
-//import Field from './Field.vue'
-//import ShowButton from './ShowButton.vue'
+import Example from './Example.vue'
+import Field from './Field.vue'
+import ShowButton from './ShowButton.vue'
 
 Vue.component ('app-block', Block)
 Vue.component ('app-example', Example)
