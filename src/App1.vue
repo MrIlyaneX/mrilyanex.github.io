d3a4a3996f6e423a9a721595e5460659bb054c9c
MrIlyaneX <45830744+MrIlyaneX@users.noreply.github.com>
2019-12-12 08:10:36 +0300

    Create App1.vue

---
+++ src/App1.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app1">
+    <app-block></app-block>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    
+    }
+  }
+}
+</script>
+
+<style >
+</style>
